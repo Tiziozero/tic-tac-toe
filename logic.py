89553aa733295d71ef36de3296c897f6fabda553
Tiziozero2 <kleidiv123@gmail.com>
2023-11-11 19:47:47 +0000

    update code

--- logic.py
+++ logic.py
@@ -1,3 +1,6 @@
+import pygame
+import settings
+
 def check_win( board, coords ):
     for row in board:
         if row[0] == row[1] == row[2]:
@@ -16,4 +19,57 @@ def check_win( board, coords ):
             if board[0][col] != 0:
                 return board[0][col], False
     return 0, True
+
+def blit_board():
+    for y in range(3):
+        for x in range(3):
+            if settings.test_board[y][x] == 0:
+                    pygame.draw.rect(settings.window,(255, 0, 0), pygame.Rect((x * 50 + 275, y * 50  ), (50, 50)))
+
+            if settings.test_board[y][x] == 1:
+                    pygame.draw.rect(settings.window,(0, 255, 0), pygame.Rect((x * 50 + 275, y * 50  ), (50, 50)))
+
+            if settings.test_board[y][x] == 2:
+                    pygame.draw.rect(settings.window,(0, 0, 255), pygame.Rect((x * 50 + 275, y * 50  ), (50, 50)))
+
+def hover_over():
+    mouse_pos = pygame.mouse.get_pos()
+    for y in range(3):
+        for x in range(3):
+            #if settings.test_board[y][x] == 0:
+                if mouse_pos[0] >= x * 50 + 275 and mouse_pos[0] <= x * 50 + 275 + 50:
+                    if mouse_pos[1] >= y * 50  and mouse_pos[1] <= y * 50 + 50:
+                                print(f"collision with {x}, {y}")
+                                return (x, y)
+
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            game_logic(hover_over(), settings.test_current_player)
+
+def update_window():
+    settings.window.blit(settings.b_image, settings.b_rect)
+    blit_board()
+    pygame.display.flip()
+
+def game_logic(pos, current_player):
+    if settings.test_board[pos[1]][pos[0]] == 0:
+        settings.test_board[pos[1]][pos[0]] = current_player
+        if settings.test_current_player == 1:
+            settings.test_current_player = 2
+        else:
+            settings.test_current_player = 1
+
+def main():
+    winning_player = 0
+    game_is_on = True
+    while game_is_on:
+        events()
+        winning_player, game_is_on = check_win(settings.test_board, (0,0))
+        update_window()
+        
+    print(f"{winning_player} won") 
+    pygame.quit()
 
--- settings.py
+++ settings.py
@@ -1,8 +1,20 @@
-#settings
+import pygame
+window = pygame.display.set_mode((800, 500))
+
+b_image = pygame.image.load("graphics/board_1.jpg").convert_alpha()
+b_rect = b_image.get_rect(center=(400, 250))
+print(f"created window, background image and background rectange")
+def setup():
+    print(f"created window, background image and background rectange")#it didn't
+
+#test settings
+
 test_board = [#col 0, 1, 2
-                 [ 1, 1, 0 ],#row 0
-                 [ 0, 1, 1 ],#row 1
-                 [ 0, 0, 2 ] #row 2
+                 [ 0, 0, 0 ],#row 0
+                 [ 0, 0, 0 ],#row 1
+                 [ 0, 0, 0 ] #row 2
              ]
 test_coords = ( 1, 2 )
-test_current_player = 2
+test_current_player = 1
+
+
