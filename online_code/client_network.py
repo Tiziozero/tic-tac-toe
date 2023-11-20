93097f0cfc0116f83caaf15151196515c8152fee
Tiziozero2 <kleidiv123@gmail.com>
2023-11-20 10:46:07 +0000

    erver stuff:w

--- online_code/client_network.py
+++ online_code/client_network.py
@@ -1,12 +1,14 @@
 import socket
 import pickle
 import threading
+import sys
 
 class Client:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip, self.port = 'localhost', 8888
         self.ongoing = True
+        self.connect()
 
     def connect(self):
         try:
@@ -14,85 +16,55 @@ class Client:
             print(f"Successfully connected to {self.ip}:{self.port}")
             message = pickle.loads(self.client.recv(1024))
             print(f"Server: {message}")
-
-            recv_thread = threading.Thread(target=self.receive, args=())
-            send_thread = threading.Thread(target=self.send, args=())
-            stop_con_thread = threading.Thread(target=self.stop_connection, args=())
-
+            recv_thread = threading.Thread(target=self.test, args=())
             recv_thread.start()
-            send_thread.start()
-            stop_con_thread.start()
-
         except socket.error as e:
             print(f"ERROR: {str(e)}")
 
     def stop_connection(self):
-        while self.ongoing:
-            try:
-                if self.client.fileno() == -1:
-                    self.ongoing = False
-                    print("connection quit from server")
-                    break
-            except KeyboardInterrupt:
-                print("Keyboard Interrupt. Closing connection")
-                self.ongoing = False
-                self.client.close()
-                break
-        print("end stop_connection thread")
-        quit()
-
+        pass
+    def test(self):
+        try:
+            while True:
+                self.send()
+                self.receive()
+        except KeyboardInterrupt:
+            print("ik")
     def receive(self):
         while self.ongoing:
-            if self.ongoing:
-                try:
-                    print("trying")
-                    data = self.client.recv(1024)
-                    decoded_data = pickle.loads(data)
-                    if decoded_data:
-                        #print(f"Received: {decoded_data}")
-                        pass
-                except socket.error:
-                    print("connection closed")
-                    break
-                except (socket.error, ConnectionResetError):
-                    print("Connection closed by the server.")
-                    self.ongoing = False
-                    self.client.close()
-                    #break
-                except pickle.UnpicklingError as e:
-                    print(f"ERROR -> UnpicklingError: {str(e)}")
-                except EOFError:
-                    print("Connection closed by the server.")
-                    self.ongoing = False
-                    self.client.close()
-                    #break
-                except Exception as e:
-                    print(f"ERROR: {str(e)}")
-                except:
-                    print("no")
+            try:
+                data = self.client.recv(1024)
+                if data:
+                    try:
+                        data = pickle.loads(data)
+                    except pickle.UnpicklingError as e:
+                        print("Unpickling Error ->", str(e))
+                        continue
+                    print(f"Received: {str(data):_<50}[]")
+                    
+                else:
+                    continue
+            except socket.error as e:
+                print(f"Socket error -> {str(e)}")
+
         print("3nd recive thread")
 
     def send(self):
         while self.ongoing:
             try:
-                coords = input("Enter coordinates (e.g., x,y): ")
-                if coords == 'quit':
-                    self.client.send(pickle.dumps("quti_server_connection"))
-                    self.ongoing = False
-                    self.client.close()
-                    break
-                coords = tuple(map(int, coords.split(',')))
-
-                self.client.sendall(pickle.dumps(coords))
-            except (socket.error, ConnectionResetError):
-                print("Connection closed by the server.")
-                self.ongoing = False
-                break
-            except Exception as e:
-                print(f"ERROR: {str(e)}")
-        print("end send thread")
+                i = input("enter coords [xy]")
+                x, y = int(i[0]), int(i[1])
+                try:
+                    data = pickle.dumps((x,y))
+                    try:
+                        self.client.send(data)
+                    except socket.error as e:
+                        print(f"Socket error -> {str(e)}")
+                except pickle.PickleError as e:
+                    print(f"picklee error -> {str(e)}")
+            except:
+                print("Invalid input")
 
 if __name__ == '__main__':
     c = Client()
-    c.connect()
 
--- server_code/server.py
+++ server_code/server.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+import sys 
 import threading
 import select
 
@@ -10,6 +11,7 @@ class Server:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip, self.port = 'localhost', 8888
         self.server.bind((self.ip, self.port))
+        self.start_server()
 
     def start_server(self):
         game_id = 0
@@ -48,82 +50,61 @@ class Game:
         self.c1 = client
         self.c2 = client_2
         self.ongoing = True
+        self.turn_player = self.c1
         print(f"Game ID: {self.game_id}, Player 1: {address}, Player 2: {address_2}")
-
+        self.start()
+    
+    def _game(self):
+        try:
+            while True:
+                print("cycle")
+                self.turn_player =self.c1
+                self.send(self.turn_player, self.game_board)
+                self.receive(self.turn_player)
+                self.changeturn()
+        except KeyboardInterrupt:
+            print("ki")
+            quit()
+    def changeturn(self):
+        if self.turn_player == self.c1:
+            self.turn_player = self.c2
+        else:
+            self.turn_player = self.c1
     def stop_connection(self):
         while self.ongoing:
-            try:
-                if self.c1._closed:
-                    self.c1.close()
-                    self.c2.close()
-                    self.server.close()
-                    print("connection from player 1 closed")
-                    break
-                if self.c2._closed:
-                    self.c1.close()
-                    self.c2.close()
-                    self.server.close()
-                    print("connection from player 2 closed")
-                    break
-            except KeyboardInterrupt:
-                print("Keyboard Interrupt. Closing connection")
-                self.ongoing = False
-                self.server.close()
-                break
-        print("end stop_connection thread")
-        quit()
+            pass
 
-    def send(self):
+    def send(self, client, data):
         while self.ongoing:
             try:
-                self.c1.sendall(pickle.dumps(self.game_board))
-                self.c2.sendall(pickle.dumps(self.game_board))
-            except (socket.error, ConnectionResetError):
-                print("Connection closed by the client.")
-                self.ongoing = False
-                break
-        print("end send thread")
+                data = pickle.dumps(data)
+                try:
+                    client.send(data)
+                    return
+                except socket.error as e:
+                    print(f"Socket eerror -> {str(e)}")
+            except pickle.UnpicklingError as e:
+                print(f"Unpicking error -> {str(e)}")
+
 
     def receive(self, client):
-        while self.ongoing:
+        while True:
             try:
                 data = client.recv(1024)
-                try:
-                    data = pickle.loads(data)
-                except:
-                    continue
-                if data == "quit_server_connection":
-                    self.ongoing = False
-                    self.c1.sendall(pickle.dumps(self.game_board))
-                    self.c2.sendall(pickle.dumps(self.game_board))
-                    self.c1.close()
-                    self.c2.close()
-                    if client == self.c1:
-                        print(f"recived message to quit server connection from {self.c1}. Quitting connection")
-                    else:
-                        print(f"recived message to quit server connection from {self.c2}. Quitting connection")
-                    self.server.close()
-                    break
-
-
-            except (socket.error, ConnectionResetError):
-                print("Connection closed by the client.")
-                self.ongoing = False
-                break
-        print("end receive thread")
-
+                if data:
+                    try:
+                        data = pickle.loads(data)
+                        print(f"received: {str(data):_<50}[]")
+                        return
+                    except pickle.PickleError as e:
+                        print(f"Pickling Error -> {str(e)}")
+            except socket.error as e:
+                    print(f"Socket eerror -> {str(e)}")
+ 
     def start(self):
         print("Game started")
-        send_thread = threading.Thread(target=self.send, args=())
-        stop_con_thread = threading.Thread(target=self.stop_connection, args=())
-        recive_thread_c1 = threading.Thread(target=self.receive, args=(self.c1,))
-        recive_thread_c2 = threading.Thread(target=self.receive, args=(self.c2,))
-        send_thread.start()
-        stop_con_thread.start()
-        recive_thread_c1.start()
-        recive_thread_c2.start()
+        _game_thread = threading.Thread(target=self._game, args=())
+        _game_thread.start()
 
 if __name__ == "__main__":
     s = Server()
-    s.start_server()
-
